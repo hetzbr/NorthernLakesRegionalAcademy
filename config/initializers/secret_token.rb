bc2e729ce20bdedfa0b4598337e248423b0b5791
hetzbr <hetzerbr@gmail.com>
2015-02-20 21:14:51 -0600

    Added refinery backend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avalon::Application.config.secret_token = '7e375f0e04071e0a31aa1de03abd8e17623906357b49bca199a8db535f6786a5fe7249006819156b5171a38c8f861fe281c54d56aa86f80e767aebe2dce79048'
